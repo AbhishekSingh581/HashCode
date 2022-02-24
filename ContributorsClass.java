7ed71f17e88eb8c0b5d550d84c96eff89e2e4e7c
Abhishek <abhiofficial581@gmail.com>
2022-02-25 00:32:29 +0530

    first commit

---
+++ ContributorsClass.java
@@ -0,0 +1,16 @@
+public class ContributorsClass{
+    String name;
+    int number;
+    public ContributorsClass(String name, int number) {
+        this.name = name;
+        this.number = number;
+    }
+    public class Skill{
+        String skillName;
+        int level;
+        public Skill(String skillName, int level) {
+            this.skillName = skillName;
+            this.level = level;
+        }
+    }
+}
